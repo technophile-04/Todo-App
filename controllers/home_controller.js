092a3f1c1853b16b202644ba08e3ea9933baa0a5
Shiv Bhonde <shivbhonde04@gmail.com>
2021-05-04 18:08:16 +0530

    set up home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,5 @@
 module.exports.home = (req, res) => {
-    return res.send('<h1>Home</h1>');
+    return res.render('home', {
+        title: 'Home'
+    });
 };
--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const port = 3000;
 const app = express();
 const mainRouter = require('./routes');
 
+// setting up view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.use('/', mainRouter);
+
 
 app.listen(port, (err) => {
     if (err) {
